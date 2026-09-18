76ac27a80e7948adcf8d358a182fcfd776f443cf
wrobit <piotrwrobel.ajiiz@gmail.com>
2026-09-18 19:51:02 +0200

    retry method function

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def main():
     print(f"Number of requests called: {request_count}")
 
 
-def count(func):
+# TODO: Add retry decorator for retrying number, raise proper exceptions using
+# try catch methods and raise exception
+# if the request is not failing then return the main function
+
+
+def count_requests(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         global request_count
@@ -21,7 +26,7 @@ def count(func):
     return wrapper
 
 
-@count
+@count_requests
 def method_to_be_counted(i):
     yield
 
