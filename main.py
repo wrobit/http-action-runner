23c90cc7dc40c2888303337b07c674be0173ec93
wrobit <piotrwrobel.ajiiz@gmail.com>
2026-09-18 19:47:31 +0200

    count method template

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+from functools import wraps
+
+request_count = 0
+
+
 def main():
-    print("Hello Wrobit :>")
+    for i in range(27):
+        method_to_be_counted(i)
+
+    print(f"Number of requests called: {request_count}")
 
 
-main()
+def count(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        global request_count
+        request_count += 1
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@count
+def method_to_be_counted(i):
+    yield
+
+
+if __name__ == "__main__":
+    main()
