44c53543ec087d45c1cc573a543f0f9d77dfd299
wrobit <piotrwrobel.ajiiz@gmail.com>
2026-09-20 19:41:49 +0200

    retry success after some failures test

--- main_test.py
+++ main_test.py
@@ -14,7 +14,7 @@ def test_count_requests_returns_original_value():
     assert test_fn() == "bar"
 
 
-def test_count_requests_increments_counter():
+def test_count_requests_increments_count_for_each_call():
     main.request_count = 0
     should_be_called_times = 5
 
@@ -22,14 +22,17 @@ def test_count_requests_increments_counter():
     def test_fn():
         return "bar"
 
-    for i in range(should_be_called_times - 1):
+    for i in range(should_be_called_times):
         test_fn()
 
     assert main.request_count == should_be_called_times
 
 
+max_retries = 3
+
+
 def test_retry_returns_original_value():
-    @main.retry(main.max_retries)
+    @main.retry(max_retries)
     @main.count_requests
     def test_fn():
         return "bar"
@@ -37,10 +40,10 @@ def test_retry_returns_original_value():
     assert test_fn() == "bar"
 
 
-def test_retry_attempt_fail():
+def test_retry_raises_runtime_error_after_all_attempts_fail():
     local_retries = 0
 
-    @main.retry(main.max_retries)
+    @main.retry(max_retries)
     def test_fn():
         nonlocal local_retries
         local_retries += 1
@@ -49,4 +52,21 @@ def test_retry_attempt_fail():
     with raises(RuntimeError, match=main.attempts_failed_error):
         test_fn()
 
-    assert local_retries == main.max_retries
+    assert local_retries == max_retries
+
+
+def test_retry_returns_value_after_some_failures():
+    local_retries = 0
+    success_after_retries = 2
+
+    @main.retry(max_retries)
+    def test_fn():
+        nonlocal local_retries
+        if local_retries == success_after_retries:
+            return "foo"
+        else:
+            local_retries += 1
+            raise requests.RequestException
+
+    assert test_fn() == "foo"
+    assert local_retries == success_after_retries
